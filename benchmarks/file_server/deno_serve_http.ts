cc33f8f7376d00a78ca8a929f34011c5496b6f98
Im-Beast <franik.mateusz@gmail.com>
2023-06-29 20:26:19 +0200

    chore: deno fmt

--- benchmarks/file_server/deno_serve_http.ts
+++ benchmarks/file_server/deno_serve_http.ts
@@ -1,35 +1,33 @@
-import { HTTP_PORT, HTTP_URL, FILE_SERVER_PATH } from "../SERVER_DATA.ts"
-import { getMimeType } from "https://deno.land/x/hono@v3.2.6/utils/mime.ts"
-import * as path from "https://deno.land/std@0.192.0/path/mod.ts"
+import { FILE_SERVER_PATH, HTTP_PORT, HTTP_URL } from "../SERVER_DATA.ts";
+import { getMimeType } from "https://deno.land/x/hono@v3.2.6/utils/mime.ts";
+import * as path from "https://deno.land/std@0.192.0/path/mod.ts";
 
 export const NAME = "Deno.serveHttp";
 export const DESCRIPTION = "";
 export const VERSION = `Deno ${Deno.version.deno}`;
 
 if (import.meta.main) {
-    for await (const conn of Deno.listen({ port: HTTP_PORT, hostname: HTTP_URL })) {
-        serveHttp(conn)
-    }
+  for await (const conn of Deno.listen({ port: HTTP_PORT, hostname: HTTP_URL })) {
+    serveHttp(conn);
+  }
 }
 
 async function serveHttp(conn: Deno.Conn) {
-    for await (const req of Deno.serveHttp(conn)) {
-        try {
-            const filePath = path.join(Deno.cwd(), FILE_SERVER_PATH, new URL(req.request.url).pathname)
-            const file = Deno.openSync(filePath)
-            const response = new Response(file.readable, {
-                headers: { 'Content-Type': getMimeType(req.request.url)! }
-            })
-            req.respondWith(response)
-        }
-        
-        catch (e) {
-            req.respondWith(
-                new Response("Not Found", {
-                    status: 200,
-                    statusText: "Not Found"
-                })
-            )
-        }
+  for await (const req of Deno.serveHttp(conn)) {
+    try {
+      const filePath = path.join(Deno.cwd(), FILE_SERVER_PATH, new URL(req.request.url).pathname);
+      const file = Deno.openSync(filePath);
+      const response = new Response(file.readable, {
+        headers: { "Content-Type": getMimeType(req.request.url)! },
+      });
+      req.respondWith(response);
+    } catch {
+      req.respondWith(
+        new Response("Not Found", {
+          status: 404,
+          statusText: "Not Found",
+        }),
+      );
     }
+  }
 }
--- benchmarks/hello_world/deno_serve_http.ts
+++ benchmarks/hello_world/deno_serve_http.ts
@@ -4,12 +4,14 @@ export const NAME = "Deno.serveHttp";
 export const DESCRIPTION = "";
 export const VERSION = `Deno ${Deno.version.deno}`;
 
-if (import.meta.main)
-    for await (const conn of Deno.listen({ port: HTTP_PORT, hostname: HTTP_URL }))
-        serveHttp(conn)
-
+if (import.meta.main) {
+  for await (const conn of Deno.listen({ port: HTTP_PORT, hostname: HTTP_URL })) {
+    serveHttp(conn);
+  }
+}
 
 async function serveHttp(conn: Deno.Conn) {
-    for await (const req of Deno.serveHttp(conn))
-        req.respondWith(new Response(RESPONSE_MESSAGE))
+  for await (const req of Deno.serveHttp(conn)) {
+    req.respondWith(new Response(RESPONSE_MESSAGE));
+  }
 }
