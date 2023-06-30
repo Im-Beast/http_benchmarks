8162ec8126961376423d14bee41d7c2fd4acdac3
Im-Beast <franik.mateusz@gmail.com>
2023-07-01 00:39:46 +0200

    feat: add Deno.serve benchmark with simple handler

--- benchmarks/file_server/deno_serve.ts
+++ benchmarks/file_server/deno_serve.ts
@@ -1,13 +1,24 @@
-import { serveDir } from "https://deno.land/std@0.192.0/http/file_server.ts";
 import { FILE_SERVER_PATH, HTTP_PORT, HTTP_URL } from "../SERVER_DATA.ts";
+import { getMimeType } from "https://deno.land/x/hono@v3.2.6/utils/mime.ts";
+import * as path from "https://deno.land/std@0.192.0/path/mod.ts";
 
-export const NAME = "Deno.serve + serveDir from std";
+export const NAME = "Deno.serve";
 export const DESCRIPTION = "";
 export const VERSION = "std 0.192.0";
 
 if (import.meta.main) {
-  await Deno.serve({ hostname: HTTP_URL, port: HTTP_PORT }, (request) =>
-    serveDir(request, {
-      fsRoot: FILE_SERVER_PATH,
-    })).finished;
+  await Deno.serve({ hostname: HTTP_URL, port: HTTP_PORT }, (request: Request) => {
+    try {
+      const filePath = path.join(Deno.cwd(), FILE_SERVER_PATH, new URL(request.url).pathname);
+      const file = Deno.openSync(filePath);
+      return new Response(file.readable, {
+        headers: { "Content-Type": getMimeType(request.url)! },
+      });
+    } catch {
+      return new Response("Not Found", {
+        status: 404,
+        statusText: "Not Found",
+      });
+    }
+  }).finished;
 }
--- benchmarks/file_server/deno_serve_serveDir.ts
+++ benchmarks/file_server/deno_serve_serveDir.ts
@@ -1,14 +1,13 @@
-import { serve } from "https://deno.land/std@0.192.0/http/server.ts";
 import { serveDir } from "https://deno.land/std@0.192.0/http/file_server.ts";
 import { FILE_SERVER_PATH, HTTP_PORT, HTTP_URL } from "../SERVER_DATA.ts";
 
-export const NAME = "std serve + serveDir";
+export const NAME = "Deno.serve + serveDir from std";
 export const DESCRIPTION = "";
-export const VERSION = "0.192.0";
+export const VERSION = "std 0.192.0";
 
 if (import.meta.main) {
-  await serve((request) =>
+  await Deno.serve({ hostname: HTTP_URL, port: HTTP_PORT }, (request) =>
     serveDir(request, {
       fsRoot: FILE_SERVER_PATH,
-    }), { hostname: HTTP_URL, port: HTTP_PORT });
+    })).finished;
 }
