6a3aa4618b58dfe0c04297757cb304beacee4f92
Im-Beast <franik.mateusz@gmail.com>
2023-07-01 20:46:34 +0200

    feat: add caching and support for other methods

--- src/mod.ts
+++ src/mod.ts
@@ -1,24 +1,51 @@
 import { crayon, join } from "./deps.ts";
 
 import { benchmarkFramework } from "./benchmarker.ts";
-import { FILE_SERVER_FILES } from "../benchmarks/SERVER_DATA.ts";
+import { FILE_SERVER_STEPS, MULTIPLE_ROUTES_STEPS } from "../benchmarks/SERVER_DATA.ts";
 import { formatResult, formatResultComparison } from "./format_results.ts";
 
 import type { BenchmarkInfo, BenchmarkResult } from "./types.ts";
 
+const BENCHMARK_CACHE_PATH = "./results/cache.json";
+
+let BENCHMARK_CACHE!: {
+  denoVersion: string;
+  data: {
+    [group: string]: {
+      [name: string]: BenchmarkResult;
+    };
+  };
+};
+
+try {
+  const content = await Deno.readTextFile(BENCHMARK_CACHE_PATH);
+  BENCHMARK_CACHE = JSON.parse(content);
+
+  if (BENCHMARK_CACHE.denoVersion !== Deno.version.deno) {
+    throw "Invalid version";
+  }
+} catch {
+  BENCHMARK_CACHE = {
+    denoVersion: Deno.version.deno,
+    data: {},
+  };
+}
+
 const BENCHMARK_PATH = "./benchmarks";
 const BENCHMARK_RESULTS_PATH = "./results";
 
-const BENCHMARK_STEPS: Record<string, [routes: [string, number][], trackSteps: boolean]> = {
-  "file_server": [FILE_SERVER_FILES, true],
-  "hello_world": [[["", 1]], false],
-};
+const BENCHMARK_STEPS: Record<string, [step: [method: string, route: string, weight: number][], trackSteps: boolean]> =
+  {
+    "file_server": [FILE_SERVER_STEPS, true],
+    "hello_world": [[["GET", "", 1]], false],
+    "multiple_routes": [MULTIPLE_ROUTES_STEPS, true],
+  };
 
 const benchmarkList: Record<string, BenchmarkInfo[]> = {};
 const benchmarkResults: Record<string, BenchmarkResult[]> = {};
 
 for await (const entry of Deno.readDir(BENCHMARK_PATH)) {
-  if (!entry.isDirectory) continue;
+  if (!entry.isDirectory || /SERVER_.+/.test(entry.name)) continue;
 
   const benchmarks = benchmarkList[entry.name] ??= [];
   const groupPath = join(BENCHMARK_PATH, entry.name);
@@ -43,32 +70,43 @@ for await (const entry of Deno.readDir(BENCHMARK_PATH)) {
   }
 }
 
-for (const benchmarkType in benchmarkList) {
-  await Deno.mkdir(join("./results", benchmarkType), { recursive: true });
+for (const benchmarkGroup in benchmarkList) {
+  await Deno.mkdir(join("./results", benchmarkGroup), { recursive: true });
 
-  const [routes, trackSteps] = BENCHMARK_STEPS[benchmarkType];
+  const [routes, trackSteps] = BENCHMARK_STEPS[benchmarkGroup];
   if (!routes) {
-    throw new Error(`Unknown benchmark type: ${benchmarkType}`);
+    throw new Error(`Unknown benchmark type: ${benchmarkGroup}`);
   }
 
-  console.log(crayon.magenta(`Starting ${crayon.bold.lightMagenta(benchmarkType)} benchmarks`));
+  console.log(crayon.magenta(`Starting ${crayon.bold.lightMagenta(benchmarkGroup)} benchmarks`));
 
-  const results = benchmarkResults[benchmarkType] ??= [];
+  const results = benchmarkResults[benchmarkGroup] ??= [];
 
-  for (const benchmark of benchmarkList[benchmarkType]) {
+  const cachedResults = BENCHMARK_CACHE.data[benchmarkGroup] ??= {};
+
+  for (const benchmark of benchmarkList[benchmarkGroup]) {
     console.log(crayon.cyan(` - Benchmarking ${crayon.bold.lightCyan(benchmark.name)}`));
 
-    const result = await benchmarkFramework(benchmark, routes, trackSteps);
-    results.push(result);
+    let result = cachedResults[benchmark.name];
+    if (result && benchmark.version === result.version) {
+      results.push(result);
+      console.log(crayon.bgLightGreen(`  - ${crayon.bold(benchmark.name)} got retrieved from cache`));
+    } else {
+      result = await benchmarkFramework(benchmark, routes, trackSteps);
+      cachedResults[benchmark.name] = result;
+      results.push(result);
+    }
 
     await Deno.writeTextFile(
-      join(BENCHMARK_RESULTS_PATH, benchmarkType, `${benchmark.fileName}.md`),
+      join(BENCHMARK_RESULTS_PATH, benchmarkGroup, `${benchmark.fileName}.md`),
       formatResult(result),
     );
   }
 
   await Deno.writeTextFile(
-    join(BENCHMARK_RESULTS_PATH, benchmarkType, "README.md"),
-    formatResultComparison(benchmarkResults[benchmarkType]),
+    join(BENCHMARK_RESULTS_PATH, benchmarkGroup, "README.md"),
+    formatResultComparison(benchmarkResults[benchmarkGroup]),
   );
 }
+
+await Deno.writeTextFile(BENCHMARK_CACHE_PATH, JSON.stringify(BENCHMARK_CACHE));
