58afbd05ae4ff1821fe39940865b4fbb0d8a8ff2
Im-Beast <franik.mateusz@gmail.com>
2023-07-03 11:46:40 +0200

    tests: add content-type and changing file tests

--- tests/file_server.test.ts
+++ tests/file_server.test.ts
@@ -1,22 +1,17 @@
-import { assertEquals, join } from "./deps.ts";
+import { assert, assertEquals, assertStringIncludes, join } from "./deps.ts";
 
-import { FILE_SERVER_PATH, FILE_SERVER_STEPS, PROTOCOL_HTTP_URL_PORT } from "../benchmarks/SERVER_DATA.ts";
+import { FILE_SERVER_PATH, PROTOCOL_HTTP_URL_PORT } from "../benchmarks/SERVER_DATA.ts";
 import { getBenchmarkList } from "../src/get_benchmarks.ts";
+import { getMimeType } from "https://deno.land/x/hono@v3.2.7/utils/mime.ts";
 
 const benchmarks = (await getBenchmarkList())["file_server"];
 
-const fileData: Record<string, Uint8Array> = {};
-
-for (const [_, fileName] of FILE_SERVER_STEPS) {
-  const filePath = join(FILE_SERVER_PATH, fileName);
-
-  const fileInfo = await Deno.stat(filePath);
-  const file = await Deno.open(filePath);
-  const buffer = new Uint8Array(fileInfo.size);
-  await file.read(buffer);
-
-  fileData[fileName] = buffer;
+let counter = 0;
+async function updateChangeFile() {
+  ++counter;
+  await Deno.writeTextFile(join(FILE_SERVER_PATH, "this-changes.txt"), `${counter}`);
 }
+await updateChangeFile();
 
 Deno.test("file_server", async (t) => {
   for (const benchmark of benchmarks) {
@@ -32,16 +27,36 @@ Deno.test("file_server", async (t) => {
       // wait for a second so deno can fully start
       await new Promise((r) => setTimeout(r, 1000));
 
-      for (const fileName in fileData) {
+      for await (const { name: fileName } of Deno.readDir(FILE_SERVER_PATH)) {
         await t.step(fileName, async () => {
+          const filePath = join(FILE_SERVER_PATH, fileName);
           const route = `${PROTOCOL_HTTP_URL_PORT}${fileName}`;
-          const readyBuffer = fileData[fileName];
 
           for (let i = 0; i < 10; ++i) {
+            await updateChangeFile();
+
+            const fileInfo = await Deno.stat(filePath);
+            const file = await Deno.open(filePath);
+            const readyBuffer = new Uint8Array(fileInfo.size);
+            await file.read(readyBuffer);
+            await file.close();
+
             const response = await fetch(route);
             const buffer = new Uint8Array(await response.arrayBuffer());
 
             assertEquals(buffer, readyBuffer);
+
+            const contentType = response.headers.get("content-type")?.split(";")?.[0];
+
+            if (contentType) {
+              assertStringIncludes(
+                getMimeType(fileName)!.toLowerCase(),
+                contentType?.toLowerCase(),
+                "Invalid content-type header",
+              );
+            } else {
+              assert(false, "Missing content-type header");
+            }
           }
         });
       }
@@ -50,4 +65,8 @@ Deno.test("file_server", async (t) => {
       await denoSubprocess.output();
     });
   }
+});
+
+addEventListener("unload", async () => {
+  await Deno.remove(join(FILE_SERVER_PATH, "this-changes.txt"));
 });
