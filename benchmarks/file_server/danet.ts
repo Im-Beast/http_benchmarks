1331c5f1b4010483b32049b0826eb631956bf10d
Im-Beast <franik.mateusz@gmail.com>
2023-07-03 18:13:45 +0200

    chore: update benchmark versions

--- benchmarks/file_server/danet.ts
+++ benchmarks/file_server/danet.ts
@@ -1,9 +1,9 @@
-import { DanetApplication, Module } from "https://deno.land/x/danet@1.7.4/mod.ts";
+import { DanetApplication, Module } from "https://deno.land/x/danet@1.8.0/mod.ts";
 import { FILE_SERVER_PATH, HTTP_PORT } from "../SERVER_DATA.ts";
 
 export const NAME = "Danet";
 export const DESCRIPTION = "";
-export const VERSION = "1.7.4";
+export const VERSION = "1.8.0";
 
 if (import.meta.main) {
   @Module({
