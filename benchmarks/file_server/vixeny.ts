a581ce25108cb9ece9696461a0f931eeda5c9e53
Im-Beast <franik.mateusz@gmail.com>
2023-07-03 19:23:15 +0200

    chore: deno fmt

--- benchmarks/file_server/vixeny.ts
+++ benchmarks/file_server/vixeny.ts
@@ -1,23 +1,19 @@
 import vixeny from "https://deno.land/x/endofunctor@v0.0.63/fun.ts";
-import { PROTOCOL_HTTP_URL_PORT, FILE_SERVER_PATH } from "../SERVER_DATA.ts";
+import { FILE_SERVER_PATH, PROTOCOL_HTTP_URL_PORT } from "../SERVER_DATA.ts";
 
 export const NAME = "Vixeny";
 export const DESCRIPTION = "";
 export const VERSION = "0.0.63";
-
 
 if (import.meta.main) {
   await Deno.serve(
     vixeny({ hasName: PROTOCOL_HTTP_URL_PORT })([
       {
         type: "static",
-        path: FILE_SERVER_PATH + "/" ,
+        path: FILE_SERVER_PATH + "/",
         name: "/",
-        // mime: false
+        mime: true,
       },
     ]),
   ).finished;
 }
-
-
-
