55bbebb045d0c75c5ae08c96defc54b97cd7ca2e
markthree <1801982702@qq.com>
2023-07-04 20:23:38 +0800

    perf(file_server/deno_serve): hoistStatic and simplify operations

--- benchmarks/file_server/deno_serve.ts
+++ benchmarks/file_server/deno_serve.ts
@@ -1,24 +1,29 @@
 import { FILE_SERVER_PATH, HTTP_PORT, HTTP_URL } from "../SERVER_DATA.ts";
 import { getMimeType } from "https://deno.land/x/hono@v3.2.7/utils/mime.ts";
-import * as path from "https://deno.land/std@0.192.0/path/mod.ts";
+import { join } from "https://deno.land/std@0.192.0/path/mod.ts";
 
 export const NAME = "Deno.serve";
 export const DESCRIPTION = "";
 export const VERSION = "std 0.192.0";
 
+const body = {
+  status: 404,
+  statusText: "Not Found",
+};
+
+const base = join(Deno.cwd(), FILE_SERVER_PATH);
+
 if (import.meta.main) {
-  await Deno.serve({ hostname: HTTP_URL, port: HTTP_PORT }, (request: Request) => {
+  await Deno.serve({ hostname: HTTP_URL, port: HTTP_PORT }, async (request: Request) => {
     try {
-      const filePath = path.join(Deno.cwd(), FILE_SERVER_PATH, new URL(request.url).pathname);
-      const file = Deno.openSync(filePath);
+      const pathname = `/${request.url.split("/").pop()}`;
+      const filePath = join(base, pathname);
+      const file = await Deno.open(filePath);
       return new Response(file.readable, {
         headers: { "Content-Type": getMimeType(request.url)! },
       });
     } catch {
-      return new Response("Not Found", {
-        status: 404,
-        statusText: "Not Found",
-      });
+      return new Response(body.statusText, body);
     }
   }).finished;
 }
