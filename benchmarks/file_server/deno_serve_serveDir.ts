711f2e965b8473ff0f70158b70e1c473df9228cb
markthree <1801982702@qq.com>
2023-07-04 20:27:14 +0800

    perf(file_server/deno_serve_serveDir): hoistStatic

--- benchmarks/file_server/deno_serve_serveDir.ts
+++ benchmarks/file_server/deno_serve_serveDir.ts
@@ -5,9 +5,10 @@ export const NAME = "Deno.serve + serveDir from std";
 export const DESCRIPTION = "";
 export const VERSION = "std 0.192.0";
 
+const opts = {
+  fsRoot: FILE_SERVER_PATH,
+};
+
 if (import.meta.main) {
-  await Deno.serve({ hostname: HTTP_URL, port: HTTP_PORT }, (request) =>
-    serveDir(request, {
-      fsRoot: FILE_SERVER_PATH,
-    })).finished;
+  await Deno.serve({ hostname: HTTP_URL, port: HTTP_PORT }, (request) => serveDir(request, opts)).finished;
 }
