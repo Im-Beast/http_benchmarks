5601d633578c4eb867583f02fdaa600f977c486d
Im-Beast <franik.mateusz@gmail.com>
2023-07-10 17:25:52 +0200

    fix: set quiet option to true in serveDir

--- benchmarks/file_server/deno_serve_serveDir.ts
+++ benchmarks/file_server/deno_serve_serveDir.ts
@@ -1,13 +1,14 @@
-import { serveDir } from "https://deno.land/std@0.192.0/http/file_server.ts";
+import { serveDir } from "https://deno.land/std@0.193.0/http/file_server.ts";
 import { FILE_SERVER_PATH, HTTP_PORT, HTTP_URL } from "../SERVER_DATA.ts";
 
 export const NAME = "Deno.serve + serveDir from std";
 export const DESCRIPTION = "";
-export const VERSION = "std 0.192.0";
+export const VERSION = "std 0.193.0";
 
 if (import.meta.main) {
   await Deno.serve({ hostname: HTTP_URL, port: HTTP_PORT }, (request) =>
     serveDir(request, {
       fsRoot: FILE_SERVER_PATH,
+      quiet: true,
     })).finished;
 }
--- benchmarks/file_server/deno_std_serve.ts
+++ benchmarks/file_server/deno_std_serve.ts
@@ -1,14 +1,15 @@
-import { serve } from "https://deno.land/std@0.192.0/http/server.ts";
-import { serveDir } from "https://deno.land/std@0.192.0/http/file_server.ts";
+import { serve } from "https://deno.land/std@0.193.0/http/server.ts";
+import { serveDir } from "https://deno.land/std@0.193.0/http/file_server.ts";
 import { FILE_SERVER_PATH, HTTP_PORT, HTTP_URL } from "../SERVER_DATA.ts";
 
 export const NAME = "std serve + serveDir";
 export const DESCRIPTION = "";
-export const VERSION = "0.192.0";
+export const VERSION = "0.193.0";
 
 if (import.meta.main) {
   await serve((request) =>
     serveDir(request, {
       fsRoot: FILE_SERVER_PATH,
+      quiet: true,
     }), { hostname: HTTP_URL, port: HTTP_PORT });
 }
