50fc63c3e7594d6cbd687d9c8c0d583b3ed9ef8b
mimiMonads <mimi@monads.dev>
2023-07-12 22:34:15 +0100

    Merge branch 'main' of https://github.com/mimiMonads/http_benchmarks v0.0.67

--- benchmarks/file_server/vixeny.ts
+++ benchmarks/file_server/vixeny.ts
@@ -1,9 +1,9 @@
-import vixeny from "https://deno.land/x/endofunctor@v0.0.63/fun.ts";
+import vixeny from "https://deno.land/x/endofunctor@v0.0.67/fun.ts";
 import { FILE_SERVER_PATH, PROTOCOL_HTTP_URL_PORT } from "../SERVER_DATA.ts";
 
 export const NAME = "Vixeny";
 export const DESCRIPTION = "";
-export const VERSION = "0.0.63";
+export const VERSION = "0.0.67";
 
 if (import.meta.main) {
   await Deno.serve(
--- benchmarks/hello_world/vixeny.ts
+++ benchmarks/hello_world/vixeny.ts
@@ -1,9 +1,9 @@
-import vixeny from "https://deno.land/x/endofunctor@v0.0.63/fun.ts";
+import vixeny from "https://deno.land/x/endofunctor@v0.0.67/fun.ts";
 import { PROTOCOL_HTTP_URL_PORT, RESPONSE_MESSAGE } from "../SERVER_DATA.ts";
 
 export const NAME = "Vixeny";
 export const DESCRIPTION = "";
-export const VERSION = "0.0.63";
+export const VERSION = "0.0.67";
 
 if (import.meta.main) {
   await Deno.serve(
