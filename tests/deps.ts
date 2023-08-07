c76176df05638c402441e4e610ddbcc9688bcad8
Im-Beast <franik.mateusz@gmail.com>
2023-08-07 10:27:31 +0200

    chore: update deps versions

--- tests/deps.ts
+++ tests/deps.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/std@0.194.0/testing/asserts.ts";
+export * from "https://deno.land/std@0.197.0/testing/asserts.ts";
 export { join, normalize } from "../src/deps.ts";
